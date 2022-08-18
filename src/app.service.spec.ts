7065fc2912fbdfb58f19b53b65a577ecde073bcc
junho100 <bemodesty306@naver.com>
2022-08-18 16:54:27 +0900

    Test: ci cd 실습을 위한 테스트 추가

---
+++ src/app.service.spec.ts
@@ -0,0 +1,13 @@
+import { AppService } from './app.service';
+
+describe('appService', () => {
+  let appService: AppService;
+  beforeEach(() => {
+    appService = new AppService();
+  });
+
+  it('test', () => {
+    const str = appService.getHello();
+    expect(str).toBe('Hello World!');
+  });
+});

ff0a59563dcec47e1c93a6ca8216b8312f16acf8
junho100 <bemodesty306@naver.com>
2022-09-02 22:16:38 +0900

    .

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,10 @@ REPOSITORY=/home/ec2-user/build
 
 cd $REPOSITORY
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+sudo su
 
-npm i
+sudo npm i
 
-pm2 kill
+sudo pm2 kill
 
-npm run start:dev
+sudo npm run start:dev

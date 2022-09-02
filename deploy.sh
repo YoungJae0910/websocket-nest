3b3ebf4fbb77a48f18898a51c628847ea815256b
junho100 <bemodesty306@naver.com>
2022-09-02 22:10:12 +0900

    .

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-sudo npm i
+npm i
 
-sudo pm2 kill
+pm2 kill
 
-sudo npm run start:dev
+npm run start:dev
